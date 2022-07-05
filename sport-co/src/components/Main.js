0b80a297efe108dde9c0bb10501de424efebc01b
jdavis19unc <89363700+jdavis19unc@users.noreply.github.com>
2022-07-04 23:06:36 -0400

    Navbar

---
+++ sport-co/src/components/Main.js
@@ -0,0 +1,14 @@
+
+function Main(){
+
+    return(
+        <div>
+            <h1>My Visualizations</h1>
+
+
+            <footer>@Jackson Davis 2022</footer>
+            </div>
+    )
+}
+
+export default Main
